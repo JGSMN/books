77474e6c21e7ba90790cbbfb3e5daa3b6f8e0cf1
JGSMN <lee49000eh@gmail.com>
2024-10-30 18:10:04 +0900

    20241030 lks

--- BookService.java
+++ BookService.java
@@ -108,6 +108,15 @@ public class BookService<PostBookResponseDto> {
                 .collect(Collectors.toList());
     }
 
+    /*
+    * 주어진 카테고리에 해당하는 책 데이터를 찾는 메서드
+    * 인터페이스에 findByCategory(Category category) 메서드가 정의되어 있어야 하며
+    * .stream 으로 메서드 결과를 스트림으로 변환후 데이터 처리를 쉽게 함
+    * .map으로 각 책 엔티티를 convertToResponseDto메서드를 book ResponseDto객체로 변환
+    * convertToResponseDto메서드를 별도의 메서드로 bookResponseDto형식으로 매핑하는 역할
+    * .collect로 변환된 bookResponseDto객체를 리스트로 수집함
+    * */
+
     // 3-3. 카테고리 & 작성자별 책 조회
     public List<bookResponseDto> getBooksByCategoryAndWriter(Category category, String writer) {
         List<Book> books;
@@ -123,6 +132,18 @@ public class BookService<PostBookResponseDto> {
                 .collect(Collectors.toList());
     }
 
+    /*
+    * 책데이터를 저장할 books 리스트를 선언후 if(category == null) 조건문을 선언함
+    * writer에 해당하는 메서드를 책 목록으로 검색함
+    * bookRepository.findByWriter(writer)메서드는
+    * writer필드를 기준으로 책을 검색하는 메서드로 bookRepository 인터페이스에 정의되어 있어야함
+    * 아니면 bookRepository.findByCategoryAndWriter(category, writer)를 호출하여
+    * 두가지조건을 충족하는 책을 조회하는데 마찬가지로 bookRepository 인터페이스에 정의되어야함
+    * books.stream으로 검색된 리스트를 스트림으로 변환후 작업편하게함
+    * .map으로 각 책 엔티티를 bookResponseDto 객체로 변환함
+    * .collect(Collectors.toList())로 변환된 객체들을 리스트로 수집하여 반환함
+    * */
+
     // 4, 특정 ID 책 수정
     public bookResponseDto updateBook(Long id, BookRequestUpdateDto updateDto) {
         Book book = bookRepository.findById(id)
@@ -136,10 +157,23 @@ public class BookService<PostBookResponseDto> {
         return convertToResponseDto(updatedBook);
     }
 
+    /*
+    * id를 기준으로 책을 조회하고 만약 해당하는 책이 존재하지 않으면
+    * 예외를 발생시키며 책을 찾을수 없다는 메시지와 id값을 포함시켜 말해줌
+    *
+    * updateDto에서 각 제목,내용,카테고리를 가져와 업데이트함
+    *
+    * 수정된 책을 bookRepository.save(book) 메서드를 통해 저장하여 업데이트된 DB에 반영함
+    * updatedBook객체를 bookResponseDto로 변환후 반환
+    * */
+
     // 5. 책 삭제
     public void deleteBook(Long id) {
         bookRepository.deleteById(id);
     }
+    /*
+    * bookRepository에서 deleteById를 호출하여 id에 해당하는 책을 삭제함
+    * */
 
     // Entity -> Response Dto 변환
     private bookResponseDto convertToResponseDto(Book book) {
@@ -150,3 +184,10 @@ public class BookService<PostBookResponseDto> {
     }
 
 }
+
+/*
+* bookResponseDto의 생성자 호출하여 Book객체의 각 필드를 bookResponseDto에 매핑함
+* book.getId(), book.getWriter(), book.getTitle()
+*   , book.getContent(), book.getCategory() 등 Book의 객체를 필드값에 추출하여
+* bookResponseDto의 필드에 할당함
+* */
